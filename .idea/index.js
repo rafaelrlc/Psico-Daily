5daf17cbe8dacb894311edd091d5c7c70f9907a4
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2023-04-16 15:28:14 -0300

    added tailwind css

--- .idea/index.js
+++ .idea/index.js
@@ -5,15 +5,14 @@ import Navbar from "../Navbar/Navbar";
 const Homepage = () => {
   const router = useRouter();
   return (
-    <div className="">
-      <Navbar />
-      <div className="flex md:flex-row flex-col justify-center items-center max-w-[1640px] lg:h-[88vh] h-[90vh] mx-auto md:gap-0 gap-10">
-        <div></div>
-        <div className="flex flex-col lg:ml-20 mx-10  lg:mt-0 items-center lg:items-start justify-center">
-          <h1 className="lg:text-4xl text-3xl font-bold text-center lg:text-left">
+    <div>
+      <Navbar></Navbar>
+      <div className="flex md:flex-row flex-col justify-center items-center md:h-[800px] h-1000px max-w-[1640px] mx-auto md:gap-0 gap-10">
+        <div className="flex flex-col md:ml-20 mx-10 mt-[5rem] md:mt-0">
+          <h1 className="text-4xl font-bold text-center md:text-left">
             Para quem é o PsicoDaily?
           </h1>
-          <h2 className="mt-5 text-center lg:text-left  font-medium text-lg w-full">
+          <h2 className="mt-5 text-center md:text-left  font-medium text-lg w-full">
             Caso esteja a procura de acompanhamento psicológico, ou deseja
             divulgar seu trabalho como profissional, o PsicoDaily é pra você!
           </h2>
@@ -26,7 +25,7 @@ const Homepage = () => {
             Registre-se agora!
           </button>
         </div>
-        <div className="hidden lg:block mt-24">
+        <div className="">
           <img
             src="https://raw.githubusercontent.com/rafaelrlc/Psico-Daily/83cb506b50cb590ee0a9b2acfc2f218867534df0/utils/rS_.svg"
             alt="homepage_page_img"
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,37 +1,92 @@
 import { useRouter } from "next/router";
-import styles from "./navbar.module.css";
 import Link from "next/link";
-
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import { MdOutlineClose } from "react-icons/md";
+import { useState } from "react";
 const Navbar = () => {
   const router = useRouter();
+  const [mobileNav, setMobileNav] = useState(false);
 
   return (
-    <div className={styles.navbar_container}>
-      <h1 className={styles.main_logo} onClick={() => router.push("/")}>
-        PsicoDaily
-      </h1>
-      <div className={styles.rightSide_options}>
-        <h2
-          onClick={() => {
-            router.push("/login");
-          }}
-        >
-          Login
-        </h2>
-        <h2
-          onClick={() => {
-            router.push("/register");
-          }}
-        >
-          Registro
-        </h2>
-        <h2>
-          <Link href={"/#about"} className={styles.aboutName}>
-            Sobre
-          </Link>
-        </h2>
+    <nav className="text-white">
+      <div className="flex items-center justify-between w-full px-14 md:h-[12vh] h-[10vh] bg-[#635acb] border-gray-200 shadow-md">
+        <div>
+          <a
+            className="flex items-center hover:cursor-pointer"
+            onClick={() => router.push("/")}
+          >
+            <span className="self-center text-[1.85rem] font-bold whitespace-nowrap hover:rotate-[-1deg] ">
+              PsicoDaily
+            </span>
+          </a>
+        </div>
+        <ul className="md:flex text-[1.4rem] space-x-8  hidden">
+          <li
+            onClick={() => router.push("/login")}
+            className="hover:cursor-pointer hover:rotate-[-1deg] "
+          >
+            <a className="block rounded hover:text-gray-300">Login</a>
+          </li>
+          <li
+            onClick={() => router.push("/register")}
+            className="hover:cursor-pointer hover:rotate-[-1deg]"
+          >
+            <a className="block rounded hover:text-gray-300">Registrar</a>
+          </li>
+          <li className="hover:cursor-pointer hover:rotate-[-1deg]">
+            <a href="#sobre" className="block rounded hover:text-gray-300">
+              Sobre
+            </a>
+          </li>
+        </ul>{" "}
+        {!mobileNav && (
+          <div
+            onClick={() => setMobileNav(!mobileNav)}
+            className="block md:hidden ml-100"
+          >
+            <div>
+              {" "}
+              <AiOutlineMenu
+                size={20}
+                className="hover:cursor-pointer"
+              ></AiOutlineMenu>
+            </div>
+          </div>
+        )}
       </div>
-    </div>
+
+      {mobileNav && (
+        <div
+          className={
+            mobileNav
+              ? "fixed left-0 top-0 w-[60%] h-full  bg-[#484199] ease-in-out duration-500 md:hidden"
+              : "fixed left-[-100%]"
+          }
+        >
+          <div className="flex items-center justify-center p-4">
+            <h1 className="w-full text-3xl font-bold text-white">PsicoDaily</h1>
+            <MdOutlineClose
+              className="text-2xl hover:cursor-pointer"
+              onClick={() => setMobileNav(!mobileNav)}
+            ></MdOutlineClose>
+          </div>
+
+          <ul className="flex flex-col gap-8 text-xl mt-5 items-start justify-center ">
+            <li
+              className={`p-4   hover:bg-[#5850ad] cursor-pointer transition-all duration-200 ease-in-out w-full`}
+            >
+              <a onClick={() => router.push("/login")}>Login</a>
+            </li>
+            <li className="p-4 hover:bg-[#5850ad] cursor-pointer transition-all duration-200 ease-in-out w-full">
+              <a onClick={() => router.push("/register")}>Registrar</a>
+            </li>
+            <li className="p-4   hover:bg-[#5850ad] cursor-pointer transition-all duration-200 ease-in-out w-full">
+              <a href="#about">Sobre</a>
+            </li>
+          </ul>
+        </div>
+      )}
+    </nav>
   );
 };
 
